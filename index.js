711676b02395a8101340e85e04343c32d925794b
petsdom <ronalvrobin28@gmail.com>
2022-08-13 00:02:18 +0300

    changes made to the prc

--- index.js
+++ index.js
@@ -6,14 +6,6 @@ const FriendModel = require('./models/Friends')
 require('dotenv').config()
 
 app.use(cors())
-app.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
-    if(req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-        return res.status(200).json({});
-    }
-    next();
-});
 app.use(express.json())
 
 mongoose.connect("mongodb+srv://petsdom:ronalgreentea@mern-petsdom.l6zpy5s.mongodb.net/?retryWrites=true&w=majority",
