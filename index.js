1807cbbf84f804c668301b54c5228cd4fada72e2
petsdom <ronalvrobin28@gmail.com>
2022-08-19 20:07:31 +0300

    hkkhh

--- index.js
+++ index.js
@@ -6,25 +6,12 @@ const FriendModel = require('./models/Friends')
 require('dotenv').config()
 
 app.use(express.json())
-app.use(cors())
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://petsdom-mern.herokuapp.com/read');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
+const corsOptions ={
+    origin:'http://petsdom-mern.tk', 
+    credentials:true,            
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions))
 mongoose.connect("mongodb+srv://petsdom:ronalgreentea@mern-petsdom.l6zpy5s.mongodb.net/?retryWrites=true&w=majority",
 { useNewUrlParser:true })
 
