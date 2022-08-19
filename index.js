a5c03539f7b10fe16404b6e6c8de5c4cb254b9f3
petsdom <ronalvrobin28@gmail.com>
2022-08-19 19:48:42 +0300

    hh

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express")
 const app = express()
+const cors = require('cors')
 const mongoose = require("mongoose")
 const FriendModel = require('./models/Friends')
 require('dotenv').config()
 
 app.use(express.json())
-
+app.use(cors())
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 mongoose.connect("mongodb+srv://petsdom:ronalgreentea@mern-petsdom.l6zpy5s.mongodb.net/?retryWrites=true&w=majority",
 { useNewUrlParser:true })
 
