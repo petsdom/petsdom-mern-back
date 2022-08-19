716e5bded6f36720e3f18c26c89a6f13eccc745d
petsdom <ronalvrobin28@gmail.com>
2022-08-19 20:01:44 +0300

    hkkh

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://petsdom-mern.vercel.app/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://petsdom-mern.herokuapp.com/read');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
