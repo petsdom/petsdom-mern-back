3b724e9140e76b9098bc58cb8725cfa6852d71c2
petsdom <ronalvrobin28@gmail.com>
2022-08-20 09:46:00 +0300

    susu

--- models/Friends.js
+++ models/Friends.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose")
 const FriendSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: false
     },
     age:{
         type: Number,
